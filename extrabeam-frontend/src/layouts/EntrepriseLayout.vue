bae6ef38391bc5ea15124e53fd43476775f0292a
aachbaro <71929000+aachbaro@users.noreply.github.com>
2025-12-16 15:39:07 +0100

    Make subscription refresh idempotent in store

---
+++ extrabeam-frontend/src/layouts/EntrepriseLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <slot />
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useSubscriptionStore } from "@/stores/subscription.store";
+
+const subscriptionStore = useSubscriptionStore();
+
+onMounted(() => {
+  void subscriptionStore.refresh();
+});
+</script>
